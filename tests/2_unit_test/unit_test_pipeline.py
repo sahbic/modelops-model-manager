7026123a4119ea01d6bcfddcec7dcd590ef24559
sahbic <sahbi.chaieb@gmail.com>
2022-02-15 14:48:26 +0100

    update test pipeline

--- tests/2_unit_test/unit_test_pipeline.py
+++ tests/2_unit_test/unit_test_pipeline.py
@@ -11,7 +11,7 @@ import unittest
 # Find the first file that matches the pattern.
 
 
-def find_file(suffix,current_dir):
+def find_file(suffix, current_dir):
     for file in os.listdir(current_dir):
         if file.endswith(suffix):
             filename = file
@@ -33,7 +33,7 @@ def load_var_names(filename, current_dir):
             names.append(row["name"])
         return names
     else:
-        print('Didnot find file: ', filename)
+        print("Didnot find file: ", filename)
         return None
 
 
@@ -43,7 +43,7 @@ def intersection(lst1, lst2):
 
 
 def load_data_by_input_vars(data, current_dir):
-    names = load_var_names('inputVar.json', current_dir)
+    names = load_var_names("inputVar.json", current_dir)
     if names is None:
         return data
     else:
@@ -55,50 +55,56 @@ def run(model_file, input_file, output_file):
     current_dir = os.path.dirname(os.path.abspath(model_file))
 
     if model_file is None:
-        print('Not found Python pickle file!')
+        print("Not found Python pickle file!")
         sys.exit()
 
     if not os.path.isfile(input_file):
-        print('Not found input file', input_file)
+        print("Not found input file", input_file)
         sys.exit()
 
     inputDf = pd.read_csv(input_file).fillna(0)
 
-    output_vars = load_var_names('outputVar.json', current_dir)
+    output_vars = load_var_names("outputVar.json", current_dir)
 
     in_dataf = load_data_by_input_vars(inputDf, current_dir)
 
-    model = open(model_file, 'rb')
+    model = open(model_file, "rb")
     pkl_model = pickle.load(model)
     model.close()
 
-    outputDf = pd.DataFrame(pkl_model.predict_proba(in_dataf))
+    outputDf = pd.DataFrame(pkl_model.predict_proba(in_dataf)).round(1)
 
     if output_vars is None:
-        outputcols = map(lambda x: 'P_' + str(x), list(pkl_model.classes_))
+        outputcols = map(lambda x: "P_" + str(x), list(pkl_model.classes_))
     else:
         outputcols = map(lambda x: output_vars[x], list(pkl_model.classes_))
     outputDf.columns = outputcols
 
     # merge with input data
-    outputDf = pd.merge(inputDf, outputDf, how='inner',
-                        left_index=True, right_index=True)
+    outputDf = pd.merge(
+        inputDf, outputDf, how="inner", left_index=True, right_index=True
+    ).round(1)
 
     # print('printing first few lines...')
     # print(outputDf.head())
-    outputDf.to_csv(output_file, sep=',', index=False)
+    outputDf.to_csv(output_file, sep=",", index=False)
     return outputDf.to_dict()
 
 
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser(description='Score')
-    parser.add_argument('-m', dest="modelFile",
-                        help='model file name, the default is the first PKL file that is found in the directory')
-    parser.add_argument('-i', dest="scoreInputCSV",
-                        required=True, help='input filename')
-    parser.add_argument('-o', dest="scoreOutputCSV",
-                        required=True, help='output csv filename')
+    parser = argparse.ArgumentParser(description="Score")
+    parser.add_argument(
+        "-m",
+        dest="modelFile",
+        help="model file name, the default is the first PKL file that is found in the directory",
+    )
+    parser.add_argument(
+        "-i", dest="scoreInputCSV", required=True, help="input filename"
+    )
+    parser.add_argument(
+        "-o", dest="scoreOutputCSV", required=True, help="output csv filename"
+    )
 
     args = parser.parse_args()
     model_file = args.modelFile
@@ -115,8 +121,8 @@ def main():
     result = run(model_file, input_file, output_file)
     return 0
 
-class ScoringTest(unittest.TestCase):
 
+class ScoringTest(unittest.TestCase):
     def setUp(self):
         self.model = sys.argv[1]
         self.input = sys.argv[2]
@@ -124,39 +130,53 @@ class ScoringTest(unittest.TestCase):
 
     def runTest_dictionary(self):
 
-        '''
+        """
         Unit test #1 for the type : dictionary
-        '''
+        """
         # print(".Running Test to check if the function generate a dictionary...")
         self.assertIsInstance(run(self.model, self.input, self.output), dict)
 
     def runTest_content(self):
 
-        '''
+        """
         Unit test #2 for the content: Not Empty
-        '''
+        """
         # print("Running Test to check if the function generate a non-empty dictionary...")
         self.assertTrue(bool(run(self.model, self.input, self.output)))
 
     def runTest_score(self):
 
-        '''
+        """
         Unit test #3 for the score: Dicts Equality
-        '''
-        
-        outdict = {'BAD': {0: 1}, 'LOAN': {0: 1100}, 'MORTDUE': {0: 25860}, 'VALUE': {0: 39025}, 'REASON': {0: 'HomeImp'},
-                    'JOB': {0: 'Other'}, 'YOJ': {0: 10.5}, 'DEROG': {0: 0}, 'DELINQ': {0: 0}, 'CLAGE': {0: 94.3666666666667}, 'NINQ': {0: 1},
-                    'CLNO': {0: 9}, 'DEBTINC': {0: 0.0}, 'P_BAD0': {0: 0.8}, 'P_BAD1': {0: 0.2}}
+        """
+
+        outdict = {
+            "BAD": {0: 1},
+            "LOAN": {0: 1100},
+            "MORTDUE": {0: 25860},
+            "VALUE": {0: 39025},
+            "REASON": {0: "HomeImp"},
+            "JOB": {0: "Other"},
+            "YOJ": {0: 10.5},
+            "DEROG": {0: 0},
+            "DELINQ": {0: 0},
+            "CLAGE": {0: 94.3666666666667},
+            "NINQ": {0: 1},
+            "CLNO": {0: 9},
+            "DEBTINC": {0: 0.0},
+            "P_BAD0": {0: 0.2},
+            "P_BAD1": {0: 0.8},
+        }
 
         # print("Running Test to check if the function score data correctly...")
         self.assertDictEqual(run(self.model, self.input, self.output), outdict)
-        
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(ScoringTest('runTest_dictionary'))
-    suite.addTest(ScoringTest('runTest_content'))
-    suite.addTest(ScoringTest('runTest_score'))
+    suite.addTest(ScoringTest("runTest_dictionary"))
+    suite.addTest(ScoringTest("runTest_content"))
+    suite.addTest(ScoringTest("runTest_score"))
     out = unittest.TextTestRunner(sys.stdout, verbosity=3).run(suite)
 
     def outcheck(outls):
